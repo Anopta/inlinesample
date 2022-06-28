ad74323cdb1300a101c518a85a4e36e5808f9d85
Serafeim Papastefanos <spapas@gmail.com>
2022-06-28 10:44:40 +0300

    Ok

--- books/views.py
+++ books/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from extra_views import (
     CreateWithInlinesView,
@@ -18,14 +17,12 @@ class BookListView(ListView):
 class EditionInline(InlineFormSetFactory):
     model = models.Edition
     fields = ["publisher", "year", "pages"]
-    extra_data = 1
     factory_kwargs = {"extra": 1}
 
 
 class TestimonialInline(InlineFormSetFactory):
     model = models.Testimonial
     fields = ["name", "testimonial"]
-    extra_data = 1
     factory_kwargs = {"extra": 1}
 
 
