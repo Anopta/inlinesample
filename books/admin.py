5151c820ee27cdf41b96488ad7634efe59961c28
Anopta <66787374+Anopta@users.noreply.github.com>
2022-07-19 11:09:24 +0900

    import requirements

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class EditionInline(admin.StackedInline):
+        model = Edition
+        extra = 0
+
+class TestimonialInline(admin.StackedInline):
+        model = Testimonial
+        extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    inlines = [EditionInline, TestimonialInline ]
+
+
+
+
